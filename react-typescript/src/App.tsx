8275d4b7ef649b50e4f61ee51ef3058989c150d6
olmosmartin <tiincho18@gmail.com>
2021-08-08 19:41:14 -0300

    componente contador sin funcionalidad creado

--- react-typescript/src/App.tsx
+++ react-typescript/src/App.tsx
@@ -1,11 +1,12 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import Contador from './components/Contador';
 
 function App() {
   return (
     <div className="App">
       inicio
+      <Contador/>
     </div>
   );
 }
---
+++ react-typescript/src/components/Contador.tsx
@@ -0,0 +1,13 @@
+import React, { ReactElement } from 'react'
+
+interface Props {
+    
+}
+
+export default function Contador({}: Props): ReactElement {
+    return (
+        <div>
+            contador
+        </div>
+    )
+}
