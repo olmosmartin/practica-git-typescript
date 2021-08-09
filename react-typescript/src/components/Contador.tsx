5624b3d7ae7eb1e7cc942b567049c7bac9560978
olmosmartin <tiincho18@gmail.com>
2021-08-09 13:45:05 -0300

    contador creado y funcionando

--- react-typescript/src/components/Contador.tsx
+++ react-typescript/src/components/Contador.tsx
@@ -1,13 +1,44 @@
-import React, { ReactElement } from 'react'
+import React, { ReactElement, useState } from 'react';
 
 interface Props {
     
 }
 
 export default function Contador({}: Props): ReactElement {
+
+    const [contador, setContador] = useState(0);
+
+    const handleButtonPress = ( numero:number ):void => {
+        setContador(contador + numero);
+    }
+
     return (
-        <div>
-            contador
+        <div className="container">
+            <h2>contador</h2>
+
+            <p>valor: {contador}</p>
+
+            <button
+            onClick={ () => handleButtonPress(1) } 
+            type="button" 
+            className="btn btn-success m-2">
+            +1
+            </button>
+
+            <button
+            onClick={ () => handleButtonPress(2) } 
+            type="button" 
+            className="btn btn-success m-2">
+            +2
+            </button>
+
+            <button
+            onClick={ () => setContador(0) } 
+            type="button" 
+            className="btn btn-danger m-2">
+            Reset
+            </button>
+
         </div>
     )
 }
