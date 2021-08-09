251838b789450911a0235be4aec5775652dac2ac
olmosmartin <tiincho18@gmail.com>
2021-08-09 14:38:09 -0300

    componente Usuario.tsx funcionando

--- react-typescript/src/components/Usuario.tsx
+++ react-typescript/src/components/Usuario.tsx
@@ -1,9 +1,37 @@
-import React, { ReactElement } from 'react'
+import React, { ReactElement, useState } from 'react'
+
+interface Usuario {
+    id:string;
+    nombre:string;
+}
+
+const usuarioInicial: Usuario = {
+    id:'0',
+    nombre:"default"
+};
 
 export default function Usuario(): ReactElement {
+
+    const [usuario, setUsuario] = useState<Usuario>(usuarioInicial);
+
+    const login = () => {
+        setUsuario({
+            id:'asd123',
+            nombre: 'Nombre Completo'
+        });
+    }
+
     return (
-        <div>
-            usuario
+        <div className="container">
+            <p> usuario</p>
+
+            <button 
+            className="btn btn-primary"
+            onClick={login}>
+                login
+            </button>
+
+            <pre>{ JSON.stringify(usuario) }</pre>
         </div>
     )
 }
